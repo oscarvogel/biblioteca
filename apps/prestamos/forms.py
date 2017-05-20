fc6ebeefc88569176370d36d0ed349491a00af5e
oscarvogel <oscarvogel@gmail.com>
2017-05-20 16:03:36 -0300

    Agregado datepicker en informe de prestamos

--- apps/prestamos/forms.py
+++ apps/prestamos/forms.py
@@ -1,7 +1,11 @@
 from django import forms
-from django.contrib.admin.widgets import AdminDateWidget 
+from datetimewidget.widgets import DateWidget
+from functools import partial
+DateInput = partial(forms.DateInput, {'class': 'datepicker'})
 
 
 class ListarPrestamosForm(forms.Form):
-    desde_fecha = forms.DateField(label='Desde Fecha',widget=forms.TextInput(attrs={'class':'datepicker'}))
-    hasta_fecha = forms.DateField(label='Hasta Fecha')
+    desde_fecha = forms.DateField(label='Desde Fecha', widget=DateInput())
+    hasta_fecha = forms.DateField(label='Hasta Fecha', widget=DateInput())
+    
+        
