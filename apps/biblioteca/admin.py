9c9c6607bfd3c601d8f64c9ff4d8a9c1406271a3
Oscar Vogel <oscarvogel@gmail.com>
2017-05-20 10:48:50 -0300

    Agregado modelo de tipo de libro, categoria y ubicacion

--- apps/biblioteca/admin.py
+++ apps/biblioteca/admin.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
-from apps.biblioteca.models import Autor, Libro
+from apps.biblioteca.models import Autor, Libro, TipoMaterial, Categorias, Ubicacion
 
 class AutorAdmin(admin.ModelAdmin):
     search_fields = ('nombre',)
 
 class LibroAdmin(admin.ModelAdmin):
+    list_filter = ('categoria__detalle',)
     search_fields = ('nombre',)
     filter_horizontal = ('autor',)
-    
+    raw_id_fields = ('tipomaterial','categoria','ubicacion')
+
+class TipoMaterialAdmin(admin.ModelAdmin):
+    search_fields = ('detalle',)
+
+class CategoriaAdmin(admin.ModelAdmin):
+    search_fields = ('detalle',)
+
+class UbicacionAdmin(admin.ModelAdmin):
+    search_fields = ('detalle',)
+
 admin.site.register(Autor, AutorAdmin)
 admin.site.register(Libro, LibroAdmin)
+admin.site.register(TipoMaterial, TipoMaterialAdmin)
+admin.site.register(Categorias, CategoriaAdmin)
+admin.site.register(Ubicacion, UbicacionAdmin)
--- apps/biblioteca/models.py
+++ apps/biblioteca/models.py
@@ -10,16 +10,46 @@ class Autor(models.Model):
     
     class Meta:
         verbose_name_plural = 'Autores'
-        
+
+class TipoMaterial(models.Model):
+    detalle = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.detalle
+
+    class Meta:
+        verbose_name_plural = 'Tipo de Materiales'
+
+class Categorias(models.Model):
+    detalle = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.detalle
+
+    class Meta:
+        verbose_name_plural = 'Categoria de libro'
+
+class Ubicacion(models.Model):
+    detalle = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.detalle
+
+    class Meta:
+        verbose_name_plural = 'Ubicacion de libro'
+
 class Libro(models.Model):
     nombre = models.CharField(max_length=80)
-    isbn = models.CharField(max_length=13)
+    isbn = models.CharField(max_length=13, blank=True)
     precio = models.DecimalField(max_digits=14,decimal_places=4,blank=True,default=0)
     detalle = models.TextField(blank=True)
     autor = models.ManyToManyField(Autor, blank=True)
     imagen = models.ImageField(upload_to = 'libro', blank=True)
     stock = models.IntegerField(default=1)
-    
+    tipomaterial = models.ForeignKey(TipoMaterial,default=1,verbose_name='Tipo de material')
+    categoria = models.ForeignKey(Categorias,default=1,verbose_name='Categoria')
+    ubicacion = models.ForeignKey(Ubicacion,default=1,verbose_name='Ubicacion')
+
     def __str__(self):
         return '%s' % (self.nombre)
     
